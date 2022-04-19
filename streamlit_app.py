34fde3875d10ee9bdfc0f933bdcf7694c79ddcb5
Albert <100301956+AlbertAit@users.noreply.github.com>
2022-04-19 15:55:30 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,4 +5,8 @@ streamlit.title ('My Mom\'s new healthy Diner')
 streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach and rocket smoothie')
+streamlit.text('Hard boiled egg')
 streamlit.text('Avocado toast')
+
+
+streamlit.header ('Build your own Fruit Smoothie')
