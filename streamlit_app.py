56c8ad67d0ba9d8bb57620fd419175c4c3c7a80a
Albert <100301956+AlbertAit@users.noreply.github.com>
2022-06-14 16:03:17 -0500

    Update streamlit_app.py
    added Emoji (line 16)

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,7 +13,7 @@ streamlit.text('🐔 Hard boiled egg')
 streamlit.text('🥑🍞 Avocado toast')
 
 
-streamlit.header ('Build your own Fruit Smoothie')
+streamlit.header ('🍌🥭 Build your own Fruit Smoothie🥝🍇')
 
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
